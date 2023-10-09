a0235e984633078bc24fe54d2bf0f4b2cf08b227
CLAUDIO CONTI RIBEIRO <claudio_ribeiro2@estudante.sc.senai.br>
2023-10-09 19:04:10 -0300

    First commit! Learning Binary Trees.

---
+++ __init__.py
@@ -0,0 +1,3 @@
+class __init__:
+    while True:
+        key = int(input("Insira uma chave"))
---
+++ insercao_em_ordem.py
@@ -0,0 +1,9 @@
+# Uma raiz, e varias chaves irão seguir
+# Criar um
+class arvore:
+    root = int(input("Insira a raiz da arvore. -9999 para sair do programa"))
+    # For com 2 chaves e verificar
+        
+        
+
+
